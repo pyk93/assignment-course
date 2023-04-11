40c716251601a31e78416caea3bf75b6075f3e57
박영규 <pyk93@hanmail.net>
2023-04-11 23:26:10 +0900

    추가 구현: bootstrap 포함

--- week5/week5/urls.py
+++ week5/week5/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from day2.views import index
 
 urlpatterns = [
     path('',index), # localhost/
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
